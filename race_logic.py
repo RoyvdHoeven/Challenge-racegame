f0b6509d9f15a2a232ff601f5ae70d9739e2eaf6
RoyvdHoeven <royvanderhoeven2208@gmail.com>
2026-03-11 15:10:48 +0100

    driving works

--- race_logic.py
+++ race_logic.py
@@ -1,4 +1,5 @@
 import os
+import keyboard
 from time import sleep
 from random import randint
 
@@ -23,13 +24,28 @@ while True:
         for tile in row:
             print(f"{tile:^2}", end="")
         print()
-    rng = randint(1, 100)
     new_road = ["[]", "[]", "[]", "[]", "[]"]
+    # check if obstacle will be placed
+    rng = randint(1, 100)
     if rng >= 30 and rng <= 40:
         random_tile = randint(0, 4)
         new_road[random_tile] = '11'
     road.insert(0, new_road)
     road.pop(7)
-    road[6][position] = 'oo'
-    sleep(1)
+    count = 0
+    pressed = False
+    # Listen for input for 0.5 seconds
+    while count < 10:
+        if keyboard.is_pressed('a') or keyboard.is_pressed('left') and not pressed:
+            position -= 1 if not position == 0 else 0
+            pressed = True
+        if keyboard.is_pressed('d') or keyboard.is_pressed('right') and not pressed:
+            position += 1 if not position == 4 else 0
+            pressed = True
+        sleep(0.05)
+        count += 1
+    if road[6][position] == '11':
+        break
+    else:
+        road[6][position] = 'oo'
     clear_console()
