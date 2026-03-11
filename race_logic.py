8205c912507afa789dbe32d25e7f508e994f2bea
RoyvdHoeven <royvanderhoeven2208@gmail.com>
2026-03-11 14:35:46 +0100

    random obstacles

--- race_logic.py
+++ race_logic.py
@@ -23,13 +23,13 @@ while True:
         for tile in row:
             print(f"{tile:^2}", end="")
         print()
-    random_tile = randint(0, 4)
+    rng = randint(1, 100)
     new_road = ["[]", "[]", "[]", "[]", "[]"]
-    new_road[random_tile] = '11'
+    if rng >= 30 and rng <= 40:
+        random_tile = randint(0, 4)
+        new_road[random_tile] = '11'
     road.insert(0, new_road)
     road.pop(7)
     road[6][position] = 'oo'
     sleep(1)
     clear_console()
-
-
